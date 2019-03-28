d8784dbdb1aa46a1ef965027614b45d60cae4316
Jack <48194176+jharmon96@users.noreply.github.com>
2019-03-28 10:28:57 -0400

    Update Main.py

--- Unanet/Main.py
+++ Unanet/Main.py
@@ -1,30 +1,38 @@
+from os import sys, path
+
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
 import time
 import project_expense_account
 import standard_functions
 import settings
-from selenium import webdriver
 from pynput.keyboard import Controller
 
 # Main.py kicks off the script
 
 
 # prompt User to choose direction of information, ie export or import
-settings.direction = input("Would you like to export or import? ")
+settings.URL = input("What is the URL? \nPaste from 'http://' until '/action'\n")
+settings.username = input("Enter the username: ")
+settings.password = input("Enter the password: ")
+settings.direction = input("Would you like to Export or Import? ")
 
 # initialize keyboard controller
 settings.keyboard = Controller()
 
 # initialize web browser used to retrieve / input information
-settings.driver = webdriver.Chrome("C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe")
+standard_functions.initialize_browser()
 
 # call the login function
 standard_functions.login()
 
 # determine which part of Unanet we are importing / exporting from
-if settings.import_type == "project_expense_account":
+if settings.importType == "project_expense_account":
+    # Kick off process
     project_expense_account.navigate()
+else:
+    "Choose a portion of the database to run this procedure on."
 
 # exit out of web browser
 time.sleep(1)
 settings.driver.quit()
-
