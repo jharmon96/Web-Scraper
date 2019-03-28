51e90db6467a348e0768a4e5fe860e7b46df431f
Jack <48194176+jharmon96@users.noreply.github.com>
2019-03-28 10:29:15 -0400

    Update settings.py

--- Unanet/settings.py
+++ Unanet/settings.py
@@ -3,8 +3,6 @@ global driver, URL, username, password, importType, file, direction
 
 # Values that provide information necessary for the script to run
 
-URL = "http://sites.unanet.com/demo_jharmon/action"
-username = "fcontroller"
-password = "welcome"
-importType = "expense/project"
+
+importType = "project_expense_account"
 file = "keys - test.csv"
